3b66dfe78faecbbbe77c31abaa96b127969901e0
Akil Turner-Richards <ki1orich@gmail.com>
2025-04-07 14:37:45 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("This program adds 2 numbers and returns the output!");
+
+Console.Write("Enter the first number: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double result = a + b;
+
+Console.WriteLine("The result is: " + result);
